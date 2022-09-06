17685e859da7a06ed3e9d4d15c7aed28ab9fbfde
tenderribs <dc3dude@gmail.com>
2022-09-06 15:34:37 +0200

    create controller for fetching all data as json

--- backend/src/api/building/controllers/building.js
+++ backend/src/api/building/controllers/building.js
@@ -6,4 +6,27 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::building.building');
+
+module.exports = createCoreController('api::building.building', ({ strapi }) => ({
+    async find(ctx) {
+        // the big boy version of getting stuff from the API:
+        //https://docs.strapi.io/developer-docs/latest/developer-resources/database-apis-reference/query-engine-api.html
+        return strapi.db.query('api::building.building').findMany({
+            populate: {
+                spaces: {
+                    populate: {
+                        measurements: true,
+                    }
+                },
+            },
+        });
+    },
+
+    async findOne(ctx) {
+        const { id } = ctx.params;
+        const { query } = ctx;
+
+        const entity = await strapi.service('api::building.building').findOne(id, query);
+        return await this.sanitizeOutput(entity, ctx);
+    }
+}));
