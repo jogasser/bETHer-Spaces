970e4c49e4f18edcb9c9e66ef1bd2a17d1b27cd4
tenderribs <dc3dude@gmail.com>
2022-09-07 12:05:47 +0200

    add new table for polygons in spaces

---
+++ backend/src/api/polygon/routes/polygon.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * polygon router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::polygon.polygon');
---
+++ backend/src/api/polygon/services/polygon.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * polygon service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::polygon.polygon');
