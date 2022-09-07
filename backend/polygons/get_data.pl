7697ebd7d50f8a8ab0234c72418a82d0f6ad074b
Johannes Gasser <johannes.gasser96@gmail.com>
2022-09-07 17:32:20 +0200

    Updated perl script to get unique coordinates

--- backend/polygons/get_data.pl
+++ backend/polygons/get_data.pl
@@ -1,17 +1,31 @@
 use strict;
 use warnings;
+use Data::Dumper;
+
 my @files = glob("*.xml");
 
+my $h = {};
+
 foreach my $file (@files) {
-	print $file, "\n";
 	open(IN, $file);
-	while(my $in = <IN>) {
-		if($in =~ /<coordinates>(.*?)<\/coordinates>/){
-			my $coords = $1;
+	my $in = join('', <IN>);
+	close(IN);
+
+	my $room = $file;
+	$room =~ s/\.xml//;
+	while($in =~ s/<coordinates>(.*?)<\/coordinates>//) {		
+		my $coords = $1;
+		if(!defined $h->{$coords}->{rooms}) {
+			$h->{$coords}->{rooms} = [];
+		}
+
+		push @{$h->{$coords}->{rooms}}, $room;
+
+		if(!defined $h->{$coords}->{points}) {
 			my @points = split(' ', $coords);
 			pop(@points);
 
-			my $polygon = "";
+			my $polygon = "\n";
 
 			for my $point ( @points) {
 				my @cords = split(',', $point);
@@ -23,10 +37,40 @@ foreach my $file (@files) {
 			}
 			chomp($polygon);
 			chop($polygon);
-
-			print $polygon, "\n";
+			$h->{$coords}->{points} = $polygon;
 		}
 	}
+}
 
-	close(IN);
+my $progress;
+my $needsFixing;
+do {
+	$progress = 0;
+	$needsFixing = 0;
+	foreach my $key(keys %$h) {
+		if(scalar @{$h->{$key}->{rooms}} == 1) {
+			my $room = @{$h->{$key}->{rooms}}[0];
+			foreach my $otherKey(keys %$h){
+				next if $otherKey eq $key;
+				my $length = scalar @{$h->{$otherKey}->{rooms}};
+				$length--;
+				#print "Array Length: ", $length, "\n";
+				my ($index) = grep { @{$h->{$otherKey}->{rooms}}[$_] eq $room } 0..$length; 
+				if (defined($index)) {
+					splice(@{$h->{$otherKey}->{rooms}}, $index, 1);  
+					$progress = 1;
+				}
+			}
+		} else {
+			$needsFixing = 1;
+		}
+	}
+} while ($needsFixing == 1);
+
+if($progress == 0) {
+	print "Failed to make further progress\n";
+}
+
+foreach my $key (keys %$h) {
+	print @{$h->{$key}->{rooms}}[0], $h->{$key}->{points}, "\n\n";
 }
